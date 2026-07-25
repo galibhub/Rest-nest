f63fccc3eb5c925890b01f7454522bd3b4b28ba8
galibhub <ibrahimgalib00@gmail.com>
2026-07-25 15:21:21 +0600

    Basic setup with necessary package installation

---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import express, { Application, Request, Response } from "express"
+import cors from "cors";
+import config from "./config";
+import cookieParser from "cookie-parser";
+const app : Application = express();
+
+//middleware
+app.use(
+  cors({
+    origin: config.app_url,
+    credentials: true,
+  }),
+);
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+
+
+
+//root route
+app.get("/",(req:Request, res: Response)=>{
+    res.send("RestNest Server is Running Now")
+})
+
+
+
+export default app;
